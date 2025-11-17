acdaf33f5b1d2c7fd252866c242781eaa0785013
Fredrik Johansson <fredrik@everyloop.com>
2025-11-17 15:10:08 +0100

    Add more examples

--- SQL/001_Select_statements.sql
+++ SQL/001_Select_statements.sql
@@ -4,6 +4,9 @@
 	Blockkommentar
 */
 
+-- Välj vilken databas som är aktiv.
+use ITHS;
+
 -- SQL är case-insensitive, d.v.s gör ingen skillnad på små och stora bokstäver,
 -- gäller data såväl som kolumnnamn, variabler etc.
 
@@ -19,18 +22,19 @@
 -- Använd en Where för att filtrera vilka rader vi tar ut, med ett (where-)vilkor.
 --select * from students where lastName = 'johansson' and id < 4;
 
--- Projection exempel:
-select 
-	id, 
-	upper(firstName) as 'FirstName', 
-	id + 100, 
-	--*, 
-	'Fredrik' as 'Fredriks namn', 
-	'Hej ' + firstName as 'Greeting',
-	firstName + ' ' + lastName as 'FullName'
 
-from 
-	Students;
+-- Projection exempel:
+--select 
+--	id, 
+--	upper(firstName) as 'FirstName', 
+--	id + 100, 
+--	--*, 
+--	'Fredrik' as 'Fredriks namn', 
+--	'Hej ' + firstName as 'Greeting',
+--	firstName + ' ' + lastName as 'FullName'
+
+--from 
+--	Students;
 
 /*
 	string literals använder 'single quotes', inte "double quotes" som i C#
@@ -46,4 +50,39 @@ from
 	or (motsvarar c# ||)
 	not (motsvarar c# !)
 */
+
+
+use everyloop;
+
+-- Keyword top begränsar antal rader man får ut.
+--select top 5 * from users;
+
+-- In
+-- select * from users where FirstName not in ('Ulla', 'Milla', 'Johanna');
+
+-- Between
+--select * from users where FirstName between 'b' and 'Caroline';
+--select * from users where FirstName >= 'b' and FirstName <= 'Caroline';
+
+-- Like
+-- select * from users where FirstName like '[a-c]%'
+
+-- Order by
+--select * from users order by FirstName asc, LastName desc
+-- select firstname from users order by len(firstname) desc;
+
+-- Case when
+--select 
+--	firstname, 
+--	case
+--		when len(firstname) < 5 then 'Short'
+--		when len(firstname) > 8 then 'Long'
+--		else 'Medium'
+--	end as 'LengthOfName'
+--from 
+--	users
+--where
+--	FirstName like '[adfk]%'
+--order by 
+--	len(firstname)
 
