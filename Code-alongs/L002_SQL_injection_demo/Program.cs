f078cf3b89fb53e3ea15e12c892747b2cc953902
Fredrik Johansson <fredrik@everyloop.com>
2025-11-24 13:53:57 +0100

    Updated with examples on Parameterized Queries

--- Code-alongs/L002_SQL_injection_demo/Program.cs
+++ Code-alongs/L002_SQL_injection_demo/Program.cs
@@ -32,7 +32,34 @@ using (var connection = new SqlConnection(connectionString))
 
 static void SearchAirports(SqlConnection connection, string searchString)
 {
-    var query = $"""
+    // SQL-INJECTION!!!
+
+    // Följande kod konkatenerar användardata in i en interpolation string som sedan skickas till SQL Server.
+    // Servern kan därmed inte gör skillnad på kod som utvecklaren skrivit och eventuell injecerad kod som användaren har skrivit.
+
+    // Ett exempel är om användaren t.ex skriver in följande kod som input:
+    // ' and 1 = 0 union select top 10 username, email, id from users; --
+
+    //var query = $"""
+    //            select top 10
+    //                [IATA],
+    //                [Airport name],
+    //                [Location served]
+    //            from 
+    //                Airports
+    //            where
+    //                [Location served] like '%{searchString}%';
+    //            """;
+
+
+    // PARAMETERIZED QUERY
+
+    // För att skydda sig mot SQL-injection får man ALDRIG konkatenera användardata och SQL-kod innan denna skickas till SQL-servern.
+    // Använd istället parameterized queries, som skickar användardata skilt från query, så server vet vilket som är vilket.
+
+    // Query bör ALLTID vara en statisk sträng
+
+    var query = """
                 select top 10
                     [IATA],
                     [Airport name],
@@ -40,11 +67,15 @@ static void SearchAirports(SqlConnection connection, string searchString)
                 from 
                     Airports
                 where
-                    [Location served] like '%{searchString}%';
+                    [Location served] like concat('%', @searchText, '%');
                 """;
 
     using (var command = new SqlCommand(query, connection))
     {
+        // Den statiska strängen ovan använder sig av en parameter @searchText, som vi kan definera som:
+        
+        command.Parameters.AddWithValue("@searchText", searchString);
+
         try
         {
             using (var reader = command.ExecuteReader())
