c48da24a77a458ea290e8ee255c468ba8535caae
Fredrik Johansson <fredrik@everyloop.com>
2025-12-08 11:01:45 +0100

    Add L004

---
+++ Code-alongs/L004_EFcore_Intro/Model/BloggingContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L004_EFcore_Intro.Model;
+
+internal class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // var connectionString = "Data Source=localhost;Database=everyloop;Integrated Security=True;TrustServerCertificate=True;";
+
+        var connectionString = new SqlConnectionStringBuilder()
+        {
+            ServerSPN = "localhost",
+            InitialCatalog = "BlogDB",
+            TrustServerCertificate = true,
+            IntegratedSecurity = true,
+        }.ToString();
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public int Rating { get; set; }
+}
+
+
---
+++ Code-alongs/L004_EFcore_Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using L004_EFcore_Intro.Model;
+using Microsoft.EntityFrameworkCore;
+
+EnsureDatabaseIsCreated();
+
+static void EnsureDatabaseIsCreated()
+{
+    using var db = new BloggingContext();
+    db.Database.EnsureCreated();
+}
