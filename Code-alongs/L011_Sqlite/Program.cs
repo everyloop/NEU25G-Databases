4cbdd1593636656daaf234fb37e2c6c2f5a5dbbe
Fredrik Johansson <fredrik@everyloop.com>
2026-01-05 19:33:37 +0100

    Add L011_Sqlite

---
+++ Code-alongs/L011_Sqlite/Program.cs
@@ -0,0 +1,30 @@
+﻿
+RecreateDatabase();
+
+AddCity("Stockholm", "Sverige");
+AddCity("Göteborg", "Sverige");
+AddCity("Malmö", "Sverige");
+AddCity("Oslo", "Norge");
+AddCity("Bergen", "Norge");
+
+static void AddCity(string cityName, string countryName)
+{
+    using var db = new SqliteContext();
+
+    var country = db.Countries.FirstOrDefault(c => c.Name == countryName) 
+        ?? new Country() { Name = countryName };
+
+    var city = new City() { Name = cityName, Country = country };
+    
+    db.Cities.Add(city);
+
+    db.SaveChanges();
+}
+
+static void RecreateDatabase()
+{
+    using var db = new SqliteContext();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+}
+
