953ac5d4d9a4700a2b00785630eab73da915415f
Fredrik Johansson <fredrik@everyloop.com>
2026-01-12 14:06:14 +0100

    Add examples with more stages, and how to create a view.

--- MongoDB/005_Aggregation_pipeline.js
+++ MongoDB/005_Aggregation_pipeline.js
@@ -1,6 +1,56 @@
 use("sample_mflix");
 
-db.movies.aggregate([
+// db.movies.aggregate([
+//     { $match: 
+//         {"year": {$gte: 2000}}
+//     },
+//     { $project: {
+//       "_id": true,
+//       "year": true,
+//       "title": true,
+//       "test": "$plot",
+//       "nextYear": { $add: ["$year", 1] },
+//       "titleWithYear": { $concat: ["$title", " (", {$toString: "$year"}, ")"] },
+//       "titleWords": {$split: ["$title", " "]},
+//       "imdb.rating": true,
+//       "imdbRating": "$imdb.rating"
+//     }},
+//     // { $lookup: {
+//     //   from: "comments",
+//     //   localField: "_id",
+//     //   foreignField: "movie_id",
+//     //   as: "comments",
+//     //   pipeline: [{$project: {
+//     //     _id: false,
+//     //     name: true,
+//     //     email: true,
+//     //     text: true
+//     //   }}]
+//     // }}
+//     { $group: {
+//       _id: "$year",
+//       "NumberOfMovies": { $count: {}},
+//       "AverageRating": { $avg: "$imdbRating"}
+//     }},
+//     { $match: {
+//       "AverageRating": { $gte: 6.5}
+//     }},
+//     { $sort: {
+//       "_id": 1
+//     }},
+//     {$skip: 2},
+//     {$limit: 5},
+//     //{$out: "myNewCollection"}
+//     // { $merge: {
+//     //   into: "myNewCollection",
+//     // //   on: 'string',
+//     // //   let: 'specification(s)',
+//     // //   whenMatched: 'string',
+//     // //   whenNotMatched: 'string'
+//     // }}
+// ]);
+
+db.createView("myView", "movies", [
     { $match: 
         {"year": {$gte: 2000}}
     },
@@ -15,18 +65,6 @@ db.movies.aggregate([
       "imdb.rating": true,
       "imdbRating": "$imdb.rating"
     }},
-    // { $lookup: {
-    //   from: "comments",
-    //   localField: "_id",
-    //   foreignField: "movie_id",
-    //   as: "comments",
-    //   pipeline: [{$project: {
-    //     _id: false,
-    //     name: true,
-    //     email: true,
-    //     text: true
-    //   }}]
-    // }}
     { $group: {
       _id: "$year",
       "NumberOfMovies": { $count: {}},
@@ -34,7 +72,12 @@ db.movies.aggregate([
     }},
     { $match: {
       "AverageRating": { $gte: 6.5}
-    }}
-]);
+    }},
+    { $sort: {
+      "_id": 1
+    }},
+    {$skip: 2},
+    {$limit: 5}
+])
 
-// select year, title, plot as 'test' from movies where year <= 1910
+// create view myView as select year, title, plot as 'test' from movies where year <= 1910
