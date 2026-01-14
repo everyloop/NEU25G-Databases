69ed4e3e1c071dd5cf6a57d6828c1eee9dd5bc66
Fredrik Johansson <fredrik@everyloop.com>
2026-01-14 13:10:08 +0100

    Updates

--- Code-alongs/L012_MongoDb.Driver/Program.cs
+++ Code-alongs/L012_MongoDb.Driver/Program.cs
@@ -5,16 +5,49 @@ var connectionString = "mongodb+srv://fredrik:fredrik@cluster0.9zffbjc.mongodb.n
 
 var client = new MongoClient(connectionString);
 
-Console.WriteLine($"Databases: {string.Join(", ", client.ListDatabaseNames().ToList())}");
+//Console.WriteLine($"Databases: {string.Join(", ", client.ListDatabaseNames().ToList())}");
 
-var database = client.GetDatabase("sample_mflix");
+//var database = client.GetDatabase("sample_mflix");
 
-Console.WriteLine($"\nCollections in database sample_mflix: {string.Join(", ", database.ListCollectionNames().ToList())}");
+//Console.WriteLine($"\nCollections in database sample_mflix: {string.Join(", ", database.ListCollectionNames().ToList())}");
 
-var collection = database.GetCollection<Movie>("movies");
+//var collection = database.GetCollection<Movie>("movies");
 
-var filter = Builders<Movie>.Filter.Empty;
+//var filter = Builders<Movie>.Filter.Empty;
+//var result = collection.Find(filter).Limit(10).ToList();
 
-var result = collection.Find(filter).Limit(10).ToList();
+//var filter = Builders<Movie>.Filter.Eq("title", "Titanic");
+//var filter = Builders<Movie>.Filter.Gte("imdb.rating", 9);
+//var filter = Builders<Movie>.Filter.In("year", new[] { 1925, 1935, 1945 });
+//var filter = Builders<Movie>.Filter.AnyEq("cast", "Bruce Willis");
+//var filter = Builders<Movie>.Filter.Regex("title", "/matrix/i");
 
-Console.WriteLine();
+//var movies = collection.Find(filter).ToList();
+
+
+//Console.WriteLine($"\nFound {movies.Count} movies:");
+
+//foreach (var movie in movies)
+//{
+//    Console.WriteLine($"{movie.Title} ({movie.Year})");
+//}
+
+
+var myUser = new User() { FirstName = "Fredrik", LastName = "Johansson" };
+
+var userCollection = client.GetDatabase("iths").GetCollection<User>("users");
+
+//userCollection.InsertOne(myUser);
+
+var userFilter = Builders<User>.Filter.Eq("FirstName", "Fredrik");
+var userUpdate = Builders<User>.Update.Set("LastName", "Gustavsson");
+
+//userCollection.UpdateMany(userFilter, userUpdate);
+
+userCollection.DeleteMany(userFilter);
+
+class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
